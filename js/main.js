48d1d367c12671d2e2dfc6f503d21e073cfba98c
Main Station <mondeageo@gmail.com>
2025-08-19 21:59:00 +0300

    Fix collapsible bug: 'collapsing when clicking the body'.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+
+collapsibles.forEach((item) => {
+  const header = item.querySelector(".collapsible__header"); // target only the header
+  header.addEventListener("click", function () {
+    item.classList.toggle("collapsible--expanded");
+  });
+});
